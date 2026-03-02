1a52eb1f94b354dc9d87bf4bed578a7abdebddb9
masroof-maindak <mujtaba.asim.amin@gmail.com>
2026-03-02 16:36:58 +0500

    fix(broker): remove traiing comma in sql query

--- broker/internal/db/queries/queries.go
+++ broker/internal/db/queries/queries.go
@@ -4,6 +4,6 @@ const SchemaCreationStatement string = `
 CREATE TABLE IF NOT EXISTS TopicMsgs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) UNIQUE NOT NULL,
-    msg VARCHAR(255) NULL,
+    msg TEXT NULL
 );
 `
